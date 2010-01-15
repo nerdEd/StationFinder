3c3ad949ebdec71ed4e00789b84f7f031e492f53
Ed Schmalzle <Jonas714@gmail.com>
2010-01-15 11:11:15 -0500

    Added GHUnit Testing Framework

---
+++ TestParsing.m
@@ -0,0 +1,32 @@
+//
+//  TestParsing.m
+//  StationFinder
+//
+//  Created by Ed Schmalzle on 1/15/10.
+//  Copyright 2010 Ed Schmalzle. All rights reserved.
+//
+
+#import "GHUnit.h"
+
+@interface TestParsing : GHTestCase { }
+@end
+
+@implementation TestParsing
+
+- (void)setUp {
+	// Run before each test method
+}
+
+- (void)tearDown {
+	// Run after each test method
+}
+
+- (void)testFail {	
+	GHAssertTrue(NO, nil);
+}
+
+- (void)testBar {
+	// Another test
+}
+
+@end
